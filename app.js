30497cf3cfc841b10485d6e56b7f3c602a36cb95
James Anthony S. Ceniza <jamesniza1@gmail.com>
2024-05-17 23:39:31 +0800

    new commit

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(cors());
 app.post('/create-checkout-session', async (req, res) => {
   const { productName, price } = req.body;
 
-  const baseUrl = 'http://34.126.113.221';
+  const baseUrl = 'http://localhost:5173';
+  //const baseUrl = 'http://34.126.113.221';
   const successUrl = `${baseUrl}/`;
   const cancelUrl = `${baseUrl}/`;
 
